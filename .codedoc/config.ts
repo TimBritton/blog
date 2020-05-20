26982d0d9823b5548806a0bdc5a0f96ce86faddc
Tim Britton <ziberath@gmail.com>
2020-05-20 17:25:52 -0500

    Update config.ts

--- .codedoc/config.ts
+++ .codedoc/config.ts
@@ -7,6 +7,7 @@ import { theme } from './theme';
 export const config = /*#__PURE__*/configuration({
   theme,                                  // --> add the theme. modify `./theme.ts` for chaning the theme.
     dest: {
+    namespace: '/blog',
     html: 'dist',
     assets: process.env.GITHUB_BUILD === 'true' ? 'dist' : '.',
     bundle: process.env.GITHUB_BUILD === 'true' ? 'bundle' : 'dist/bundle',
