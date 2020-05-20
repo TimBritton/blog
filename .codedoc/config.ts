5879057b3d88cc3f5f25c79caa5e52b381a77c9b
Tim Britton <ziberath@gmail.com>
2020-05-20 13:10:12 -0500

    Update config.ts

--- .codedoc/config.ts
+++ .codedoc/config.ts
@@ -6,7 +6,11 @@ import { theme } from './theme';
 
 export const config = /*#__PURE__*/configuration({
   theme,                                  // --> add the theme. modify `./theme.ts` for chaning the theme.
-  
+  dest: {
+    // ...
+    html: 'dist',
+    assets: 'dist',
+  },
   page: {
     title: {
       base: 'Blog'                        // --> the base title of your doc pages
